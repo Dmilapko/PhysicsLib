9dad1e0596dc2b1661af09f02a2df90ad1fec28e
Dmilapko <68218175+Dmilapko@users.noreply.github.com>
2022-06-25 21:06:12 +0300

    Introduced deathpoints, introduced PointD, switched to full-metric, introduced int-oriented matrix

--- PhysicsLib/MatrixPhysics.cs
+++ PhysicsLib/MatrixPhysics.cs
@@ -13,21 +13,39 @@ namespace Physics
     {
         public class MP_Object : PhysicsObject
         {
-            public List<PointF> points = new List<PointF>();
-            public float x, y, realx, realy;
-            public float dir = 0;
+            /// <summary>
+            /// Physics points in PIXELS
+            /// </summary>
+            public List<PointD> points = new List<PointD>();
+            /// <summary>
+            /// Death points in PIXELS
+            /// </summary>
+            public List<PointD> deathpoints = new List<PointD>();
+            public PointD position;
+            public double rotation = 0;
             public List<Force> forces = new List<Force>();
+            public bool alive = true;
 
-            public MP_Object(float _MomentOfInertia, float _mass, PointF _position, List<PointF> _points)
+            public MP_Object(double _MomentOfInertia, double _mass, PointD _position)
             {
                 MonentOfInertia = _MomentOfInertia;
                 mass = _mass;
-                x = _position.X;
-                y = _position.Y;
-                points = _points;
+                position = _position;
+                InitializePoints();
+                InitializeDeathPoints();
             }
 
-            public override void ApplyForce(PointF pos, float angle, float force)
+            public virtual void InitializePoints()
+            {
+                List<PointD> points = new List<PointD>();
+            }
+
+            public virtual void InitializeDeathPoints()
+            {
+                List<PointD> deathpoints = new List<PointD>();
+            }
+
+            public override void ApplyForce(PointD pos, double angle, double force)
             {
                 base.ApplyForce(pos, angle, force);
                 forces.Add(new Force(pos, angle, force));
@@ -40,27 +58,72 @@ namespace Physics
         }
 
         public List<MP_Object> objects;
-        public List<List<bool>> matrix;
-        public float pim;
+        public List<int> matrix = new List<int>();
+        public double pim;
         public int fps;
+        PointD BottomLeft, TopRight;
 
-        public MatrixPhysics(List<MP_Object> _objects, List<List<bool>> _matrix, float pixels_in_meter, int _fps)
+        public void SetMatrix(List<List<bool>> _matrix)
+        {
+            matrix.Clear();
+            for (int x = 0; x < _matrix.Count; x++)
+            {
+                int height;
+                for (height = 0; height < 1080; height++)
+                {
+                    if (!_matrix[x][height]) break;
+                }
+                matrix.Add(height);
+            }
+        }
+
+        public MatrixPhysics(List<MP_Object> _objects, PointD _BottomLeft, PointD _TopRight, double pixels_in_meter, int _fps)
         {
             objects = _objects;
-            matrix = _matrix;
             pim = pixels_in_meter;
             fps = _fps;
+            BottomLeft = _BottomLeft;
+            TopRight = _TopRight;
+        }
+
+        public PointD GetMatrixPosition(PointD point, PointD position, double rotation)
+        {
+            double l = Math.Sqrt(point.X * point.X + point.Y * point.Y);
+            double d = rotation + (double)Math.Atan2(point.X, point.Y);
+            return new PointD(position.X + Math.Sin(d) * l, position.Y + Math.Cos(d) * l);
+        }
+
+        public bool GetMatrixState(PointD position)
+        {
+            if (position.ToVector().InRect(BottomLeft.ToVector(), TopRight.ToVector()))
+            {
+                position.X *= pim;
+                position.Y *= pim;
+                return (position.Y < matrix[(int)Math.Round(position.X)]);
+            }
+            else return false;
         }
 
         private void RunObjPhysics(MP_Object obj)
         {
-            obj.forces.Clear();
-            obj.Run();
-            //float nextx, nexty;
-            obj.dir += obj.angularvelocity / (float)fps;
-            obj.x += obj.speed.X * pim / (float)fps;
-            obj.y += obj.speed.Y * pim / (float)fps;
-            obj.dir = obj.dir % 360.ToRadians();
+            if (obj.alive)
+            {
+                obj.forces.Clear();
+                obj.Run();
+                //float nextx, nexty;
+                obj.rotation += obj.angularvelocity / (float)fps;
+                obj.position.X += obj.speed.X / (float)fps;
+                obj.position.Y += obj.speed.Y / (float)fps;
+                obj.rotation = obj.rotation % 360.ToRadians();
+                foreach (PointD point in obj.deathpoints)
+                {
+                    if (GetMatrixState(GetMatrixPosition(point, obj.position, obj.rotation)))
+                    { 
+                        obj.alive = false;
+                        break;
+                    }
+                }
+            }
         }
 
         public void Run()
--- PhysicsLib/PhysicsObject.cs
+++ PhysicsLib/PhysicsObject.cs
@@ -10,9 +10,9 @@ namespace Physics
 {
     public class Force
     {
-        public PointF pos; public float angle; public float force;
+        public PointD pos; public double angle; public double force;
 
-        public Force(PointF _pos, float _angle, float _force)
+        public Force(PointD _pos, double _angle, double _force)
         {
             pos = _pos;
             angle = _angle;
@@ -24,19 +24,19 @@ namespace Physics
     {
         //public delegate void MOIF();
         //public MOIF MonentOfInertia;
-        public float MonentOfInertia;
+        public double MonentOfInertia;
         /// <summary>
         /// Angular speed in degrees
         /// </summary>
-        public float angularvelocity = 0;
+        public double angularvelocity = 0;
         /// <summary>
         /// Mass of object in kilos
         /// </summary>
-        public float mass;
+        public double mass;
         /// <summary>
         /// Vector of speed
         /// </summary>
-        public PointF speed = new PointF(0, 0);
+        public PointD speed = new PointD(0, 0);
 
         public PhysicsObject()
         {
@@ -55,14 +55,14 @@ namespace Physics
         /// <param name="pos"></param>
         /// <param name="angle">Angle in radians</param>
         /// <param name="force"></param>
-        public virtual void ApplyForce(PointF pos, float angle, float force)
+        public virtual void ApplyForce(PointD pos, double angle, double force)
         {
             //angle - Math.Atan2(pos.X, pos.Y) angle between ANGLE and tangent
-            float torque = (float)(force * Math.Sqrt(pos.X * pos.X + pos.Y * pos.Y) * Math.Sin(angle - Math.Atan2(pos.X, pos.Y)));
+            double torque = (force * Math.Sqrt(pos.X * pos.X + pos.Y * pos.Y) * Math.Sin(angle - Math.Atan2(pos.X, pos.Y)));
             angularvelocity += (torque / MonentOfInertia);
 
-            speed.X += force * (float)Math.Sin(angle) / mass;
-            speed.Y += force * (float)Math.Cos(angle) / mass;
+            speed.X += (float)(force * Math.Sin(angle) / mass);
+            speed.Y += (float)(force * Math.Cos(angle) / mass);
         }
     }
 }
